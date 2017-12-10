dd34de00a08ac98fb9e51b9fc69b9aaae83b55ee
husky <956283490@qq.com>
2017-12-10 13:15:58 +0800

    'change'

--- src/components/project/3.vue
+++ src/components/project/3.vue
@@ -1,11 +1,20 @@
 <template>
-  <div >
-    3
+  <div id="warp">
+    <div id="allmap"></div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  mounted() {
+
+  }
 }
 </script>
+<style>
+  #allmap, #warp {
+    width: 100%;
+    height: 100%;
+  }
+</style>
